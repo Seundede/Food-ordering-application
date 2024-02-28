a9e2550c465d73459ae6a21c1fec08e6bfe83f2e
Seundede <omadoyedede0@gmail.com>
2024-02-28 22:20:34 +0100

    refactor: flat list implementation

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,21 @@
 import ProductListItem from "@/src/components/ProductListItem";
-import { View } from "@/src/components/Themed";
+import {
+  SafeAreaView,
+  FlatList,
+} from "react-native";
 import products from "@/assets/data/products";
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <ProductListItem product={products[5]} />
-      <ProductListItem product={products[1]} />
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={products}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+     numColumns={2}
+     contentContainerStyle={{gap:10, padding:10}}
+     columnWrapperStyle={{gap:10}}
+      />
+    </SafeAreaView>
   );
 }
 
--- src/components/ProductListItem.tsx
+++ src/components/ProductListItem.tsx
@@ -11,7 +11,7 @@ type ProductListItemProps = {
 const ProductListItem = ({ product }: ProductListItemProps) => {
   return (
     <View
-      style={tw`bg-white border border-white rounded-xl p-3 overflow-hidden`}
+      style={tw`bg-white flex-1 max-w-1/2  border border-white rounded-xl p-3 overflow-hidden`}
     >
       <Image
         source={{
