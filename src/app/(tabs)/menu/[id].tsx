e30c2621c93fbefddcbf303b2f67a8233d965281
Seundede <omadoyedede0@gmail.com>
2024-02-29 21:14:25 +0100

    feat: product item component

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,19 +1,46 @@
-import { Text, SafeAreaView } from "react-native";
+import { Text, SafeAreaView, Image, View } from "react-native";
 import React from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router';
+import products from "@/assets/data/products";
+import tw from "twrnc";
 
-
+const sizes = ['S','M','L','XL']
 const ProductDetail = () => {
   const { id } = useLocalSearchParams();
+  const product = products.find((p) => p.id.toString() == id)
+  if(!product) {
+    return <Text>Product not found</Text>
+  }
   return (
-    <SafeAreaView>
+    <View style={tw`bg-white flex-1 p-3`}>
       <Stack.Screen
         options={{
           headerShown: false,
         }}
       />
-      <Text>ProductDetail {id}</Text>
-    </SafeAreaView>
+      <Image
+        source={{
+          uri:
+            product.image ||
+            "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
+        }}
+        style={tw`w-full aspect-square`}
+        resizeMode="contain"
+      />
+      <Text style={tw`text-base`}>Select Size</Text>
+
+      <View style={tw`flex flex-row justify-around my-3`}>
+        {sizes.map((size) => (
+          <View
+            key={size}
+            style={tw`bg-gray-200 rounded-full w-12 aspect-square items-center justify-center`}
+          >
+            <Text style={tw`text-lg font-bold`}>{size}</Text>
+          </View>
+        ))}
+      </View>
+      <Text style={tw`text-lg font-bold`}>${product.price}</Text>
+    </View>
   );
 }
 
