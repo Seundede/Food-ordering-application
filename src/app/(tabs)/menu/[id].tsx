4426f49433b5f69738a17bfe6565c9ff99047870
Seundede <omadoyedede0@gmail.com>
2024-03-07 23:01:02 +0100

    feat: item size feature

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,15 +1,16 @@
-import { Text, SafeAreaView, Image, View } from "react-native";
-import React from 'react'
-import { Stack, useLocalSearchParams } from 'expo-router';
+import { Text, Image, View, Pressable } from "react-native";
+import React, { useState } from "react";
+import { Stack, useLocalSearchParams } from "expo-router";
 import products from "@/assets/data/products";
 import tw from "twrnc";
 
-const sizes = ['S','M','L','XL']
+const sizes = ["S", "M", "L", "XL"];
 const ProductDetail = () => {
   const { id } = useLocalSearchParams();
-  const product = products.find((p) => p.id.toString() == id)
-  if(!product) {
-    return <Text>Product not found</Text>
+  const [selectedSize, setSelectedSize] = useState("M");
+  const product = products.find((p) => p.id.toString() == id);
+  if (!product) {
+    return <Text>Product not found</Text>;
   }
   return (
     <View style={tw`bg-white flex-1 p-3`}>
@@ -26,23 +27,33 @@ const ProductDetail = () => {
             "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/food/default.png",
         }}
         style={tw`w-full aspect-square`}
-        resizeMode="contain"
       />
       <Text style={tw`text-base`}>Select Size</Text>
 
       <View style={tw`flex flex-row justify-around my-3`}>
         {sizes.map((size) => (
-          <View
+          <Pressable
             key={size}
-            style={tw`bg-gray-200 rounded-full w-12 aspect-square items-center justify-center`}
+            onPress={() => setSelectedSize(size)}
+            style={[
+              tw`rounded-full w-12 aspect-square items-center justify-center `,
+              tw`${selectedSize === size ? "bg-gray-300" : "bg-white"}`,
+            ]}
           >
-            <Text style={tw`text-lg font-bold`}>{size}</Text>
-          </View>
+            <Text
+              style={[
+                tw`text-lg font-bold`,
+                tw`${selectedSize === size ? "text-black" : "text-slate-400"}`,
+              ]}
+            >
+              {size}
+            </Text>
+          </Pressable>
         ))}
       </View>
       <Text style={tw`text-lg font-bold`}>${product.price}</Text>
     </View>
   );
-}
+};
 
-export default ProductDetail
+export default ProductDetail;
