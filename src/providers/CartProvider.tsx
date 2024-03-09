580f7632fd9ffdd769d44a296c7e0eff0ece8d4f
Seundede <omadoyedede0@gmail.com>
2024-03-09 21:56:29 +0100

    feat: update quantity functionality

--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,31 +1,53 @@
-import React, {
-  PropsWithChildren,
-  createContext,
-  useContext,
-  useState,
-} from "react";
+import React, { PropsWithChildren, createContext, useState } from "react";
 import { CartItem, Product } from "../types";
+import { randomUUID } from "expo-crypto";
 
-// Define the type for the context value
+// Type for the context value
 interface CartContextType {
   items: CartItem[];
   addItem: (product: Product, size: CartItem["size"]) => void;
+  updateQuantity: (itemId: string, amount: -1 | 1) => void;
 }
 // Create the context with an initial value
 export const CartContext = createContext<CartContextType>({
   items: [],
   addItem: () => {},
+  updateQuantity: () => {},
 });
-
-
 
 const CartProvider = ({ children }: PropsWithChildren) => {
   const [items, setItems] = useState<CartItem[]>([]);
+  //Function to add item to cart
   const addItem = (product: Product, size: CartItem["size"]) => {
-    console.log(product);
+    const existingItem = items.find((item) => item.product === product && item.size === size)
+    if(existingItem) {
+        updateQuantity(existingItem.id,1)
+        return
+    }
+    const ProductItem: CartItem = {
+      product,
+      size,
+      quantity: 1,
+      product_id: product.id,
+      id: randomUUID(),
+    };
+    setItems([ProductItem, ...items]);
   };
+//Function to update (increase or decrease) item quantity
+  const updateQuantity = (itemId: string, amount: -1 | 1) => {
+    setItems(
+      items
+        .map((item) =>
+          item.id !== itemId
+            ? item
+            : { ...item, quantity: item.quantity + amount }
+        )
+        .filter((item) => item.quantity > 0)
+    );
+  };
+
   return (
-    <CartContext.Provider value={{ items, addItem}}>
+    <CartContext.Provider value={{ items, addItem, updateQuantity }}>
       {children}
     </CartContext.Provider>
   );
