93f1597de0ead88207abb923e76f25fb1f8596a8
Seundede <omadoyedede0@gmail.com>
2024-03-09 21:25:10 +0100

    feat: cart list item component

---
+++ src/components/CartListItem.tsx
@@ -0,0 +1,58 @@
+import { View, Text, Image, Pressable } from "react-native";
+import React, { useContext } from "react";
+import Colors from "../constants/Colors";
+import { CartItem } from "../types";
+import { Link } from "expo-router";
+import tw from "twrnc";
+import { FontAwesome } from "@expo/vector-icons";
+import { CartContext } from "../providers/CartProvider";
+const { tint } = Colors.light;
+
+type CartListItemProps = {
+  cartItem: CartItem;
+};
+
+const CartListItem = ({ cartItem }: CartListItemProps) => {
+//   const { updateQuantity } = useContext(CartContext);
+  return (
+    <View style={tw`bg-white rounded-xl p-2 flex-1 flex-row items-center`}>
+      <Image
+        source={{ uri: cartItem.product.image || "" }}
+        style={tw`aspect-square self-center w-20 mr-3`}
+        resizeMode="contain"
+      />
+      <View style={{ flex: 1 }}>
+        <Text style={tw`font-medium text-base mb-2`}>
+          {cartItem.product.name}
+        </Text>
+        <View style={tw`flex-row gap-2`}>
+          <Text style={[{ color: tint }, tw`font-semibold`]}>
+            ${cartItem.product.price.toFixed(2)}
+          </Text>
+          <Text>Size: {cartItem.size}</Text>
+        </View>
+      </View>
+      <View style={tw`flex-row gap-2 items-center my-2`}>
+        <FontAwesome
+          //   onPress={() => updateQuantity(cartItem.id, -1)}
+          name="minus"
+          color="gray"
+          style={{ padding: 5 }}
+        />
+
+        <Text style={tw`font-medium`}>{cartItem.quantity}</Text>
+        <FontAwesome
+          //   onPress={() => updateQuantity(cartItem.id, 1)}
+          name="plus"
+          color="gray"
+          style={{ padding: 5 }}
+        />
+      </View>
+    </View>
+  );
+};
+
+
+
+
+export default CartListItem;
