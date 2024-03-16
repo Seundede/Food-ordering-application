4e53cc9c0998c2c0736dff79ed50a40ede06cf1c
Seundede <omadoyedede0@gmail.com>
2024-03-16 21:08:40 +0100

    feat: oder list item component

---
+++ src/components/OrderListItem.tsx
@@ -0,0 +1,40 @@
+import { View, Text,  Pressable } from "react-native";
+import React from "react";
+import { Order } from "../types";
+import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "dayjs";
+import { Link, useSegments } from "expo-router";
+import tw from "twrnc";
+
+dayjs.extend(relativeTime);
+
+type OrderListItemProps = {
+  order: Order;
+};
+
+const OrderListItem = ({ order }: OrderListItemProps) => {
+  const segments = useSegments();
+
+  return (
+    <Link
+      href={
+        segments[0] === "(admin)"
+          ? `/(admin)/orders/${order.id}`
+          : `/(user)/orders/${order.id}`
+      }
+      asChild
+    >
+      <Pressable style={tw`bg-white p-3 rounded-md flex-row justify-between items-center`}>
+        <View>
+          <Text style={tw`font-semibold my-2`}>Order #{order.id}</Text>
+          <Text style={tw`text-gray-300`}>{dayjs(order.created_at).fromNow()}</Text>
+        </View>
+
+        <Text style={tw`font-medium`}>{order.status}</Text>
+      </Pressable>
+    </Link>
+  );
+};
+
+
+export default OrderListItem;
