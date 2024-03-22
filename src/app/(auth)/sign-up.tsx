41abb7c0dd9423feb3b2b76ae43cb7ed4d2bbe82
Seundede <omadoyedede0@gmail.com>
2024-03-22 01:43:31 +0100

    feat: supabase sign up with email functionality

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,27 +1,40 @@
-import { View, Text, TextInput} from "react-native";
+import { View, Text, TextInput, Alert } from "react-native";
 import React, { useState } from "react";
 import Button from "../../components/Button";
 import Colors from "../../constants/Colors";
 import { Link, Stack } from "expo-router";
 import tw from "twrnc";
+import { supabase } from "@/src/lib/supabase";
 
 const SignUpScreen = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
 
+  //Function to sign a user up
+  async function signUpWithEmail() {
+    setLoading(true);
+    const { error } = await supabase.auth.signUp({
+      email,
+      password,
+    });
+    if (error) Alert.alert(error.message);
+    setLoading(false);
+  }
   return (
     <View style={tw`p-5 flex-1 justify-center`}>
       <Stack.Screen options={{ title: "Sign up" }} />
 
-      <Text style={tw`text-gray-200`}>Email</Text>
+      <Text style={tw`text-gray-800`}>Email</Text>
       <TextInput
         value={email}
         onChangeText={setEmail}
         placeholder="jon@gmail.com"
+        autoCapitalize="none"
         style={tw`border border-gray-400 p-3 mt-2 mb-5 bg-white rounded-md`}
       />
 
-      <Text style={tw`text-gray-200`}>Password</Text>
+      <Text style={tw`text-gray-800`}>Password</Text>
       <TextInput
         value={password}
         onChangeText={setPassword}
@@ -30,7 +43,11 @@ const SignUpScreen = () => {
         secureTextEntry
       />
 
-      <Button text="Create account" />
+      <Button
+        text={loading ? "Creating account ..." : "Create account"}
+        onPress={signUpWithEmail}
+        disabled={loading}
+      />
       <Link
         href="/sign-in"
         style={[tw`self-center font-medium my-3`, { color: Colors.light.tint }]}
@@ -40,7 +57,5 @@ const SignUpScreen = () => {
     </View>
   );
 };
-
-
 
 export default SignUpScreen;
