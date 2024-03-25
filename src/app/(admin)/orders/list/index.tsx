4c05fb2391735ab19ea9b5a4cb342c2af8d656de
Seundede <omadoyedede0@gmail.com>
2024-03-25 21:50:26 +0100

    chore: update new order immedaitely in the application

--- src/app/(admin)/orders/list/index.tsx
+++ src/app/(admin)/orders/list/index.tsx
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ActivityIndicator, FlatList, Text } from "react-native";
 import OrderListItem from "@/src/components/OrderListItem";
 import { useAdminOrderList } from "@/src/api/orders";
+import { supabase } from "@/src/lib/supabase";
+import { useQueryClient } from "@tanstack/react-query";
 
 const OrderScreen = () => {
-  const { data: orders, isLoading, error } = useAdminOrderList({archived : false});
-
+  const {
+    data: orders,
+    isLoading,
+    error,
+  } = useAdminOrderList({ archived: false });
+  const queryClient = useQueryClient();
+  useEffect(() => {
+    const ordersSubscription = supabase
+      .channel("custom-insert-channel")
+      .on(
+        "postgres_changes",
+        { event: "INSERT", schema: "public", table: "orders" },
+        (payload) => {
+          queryClient.invalidateQueries({ queryKey: ["orders"] });
+          queryClient.refetchQueries({ queryKey: ["orders"], type: "active" });
+        }
+      )
+      .subscribe();
+    return () => {
+      ordersSubscription.unsubscribe();
+    };
+  }, []);
   if (isLoading) {
     return <ActivityIndicator />;
   }
